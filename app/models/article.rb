8a57f22f9d0abcf64d212069f939e8e97675000a
LedisHzj <lxotzai013@gmail.com>
2019-02-25 18:06:10 +0200

    styling edit end show

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
-  validates_presence_of :title, :description, length:{minimum: 5, maximum: 15}
-  validates :text, length: {minimum: 10, maximum: 200}
+  validates_presence_of :title, :description
+  validates_length_of :title, minimum: 5 , maximum: 100
+  validates_length_of :description, maximum: 255
+  validates :text, length: {minimum: 20}
 end
