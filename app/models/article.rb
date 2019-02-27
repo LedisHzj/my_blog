fdb3de73e8185c059e52b05b1a4f841a4fc304a2
LedisHzj <lxotzai013@gmail.com>
2019-02-27 16:56:25 +0200

    second up today

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,6 @@ class Article < ApplicationRecord
   validates_length_of :title, minimum: 5 , maximum: 100
   validates_length_of :description, maximum: 255
   validates :text, length: {minimum: 20}
+
+  belongs_to :user
 end
