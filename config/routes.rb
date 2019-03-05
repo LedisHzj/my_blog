897041feede0c00f0128a0f3d8b9a50a4ec95715
LedisHzj <lxotzai013@gmail.com>
2019-03-05 15:58:40 +0200

    fixing user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   resources :articles
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+
+  devise_for :users, controllers: { sessions: 'users/sessions',
+                                              devise: 'users/devise',
+                                              passwords: 'users/passwords',
+                                              unlocks: 'users/unlocks',
+                                              registrations: 'users/registrations'}
+
+  resources :users, except:[:new]
 
   root 'pages#home'
   get 'about', to: 'pages#about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'signup', to: 'users#new'
+  # #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
